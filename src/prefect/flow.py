5f210a97186074b5787ec47ae470790da6836f62
Leonel481 <leonel.aliaga.v@gmail.com>
2025-08-14 21:39:33 -0500

    update flow models params

--- src/prefect/flow.py
+++ src/prefect/flow.py
@@ -2,6 +2,12 @@ import sys
 import os
 # sys.path.append("/app")
 
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from xgboost import XGBClassifier
+from lightgbm import LGBMClassifier
+from hyperopt import hp, scope
+
 from prefect import flow, task
 from src.pipeline.transform import TransformData
 from src.pipeline.model_train import ModelTrain
@@ -40,6 +46,23 @@ def task_ETL_data(path_ini: str, path_end: str, df_override: pd.DataFrame = None
 @task
 def task_train_model(project, bucket, path_data_process: str, path_artifacts: str, path_models: str, path_metrics:str, models, param_space):
 
+    models = {
+        "LogisticRegression": LogisticRegression(max_iter=1000, random_state=42),
+        "RandomForest": RandomForestClassifier(random_state=42),
+        "XGBoost": XGBClassifier(use_label_encoder=False, eval_metric='logloss', random_state=42),
+        "LightGBM": LGBMClassifier(random_state=42)
+    }
+    
+    param_space = {
+        'max_depth': scope.int(hp.quniform('max_depth', 4, 15, 1)),
+        'learning_rate': hp.loguniform('learning_rate', -3, 0),
+        'reg_alpha': hp.loguniform('reg_alpha', -5, -1),
+        'reg_lambda': hp.loguniform('reg_lambda', -6, -1),
+        'min_child_weight': hp.loguniform('min_child_weight', -1, 3),
+        'subsample': hp.uniform('subsample', 0.6, 1.0),
+        'colsample_bytree': hp.uniform('colsample_bytree', 0.6, 1.0)
+    }
+
     trainer = ModelTrain(project = project, bucket = bucket,
                          path_artifacts = path_artifacts,
                          path_models = path_models, 
@@ -77,11 +100,11 @@ def task_train_model(project, bucket, path_data_process: str, path_artifacts: st
 
 # --- Flow principal ---
 @flow(name="ML Pipeline")
-def ml_pipeline(project, bucket, path_ini, path_end, path_artifacts, path_models, path_metrics, models, param_space, df_override=None, **kwargs):
+def ml_pipeline(project, bucket, path_ini, path_end, path_artifacts, path_models, path_metrics, df_override=None, **kwargs):
     clean_data_path = task_ETL_data(path_ini, path_end, df_override = df_override, **kwargs)
     task_train_model(project, bucket, path_data_process = clean_data_path, 
                      path_artifacts = path_artifacts, path_models = path_models, 
-                     path_metrics = path_metrics, models = models, param_space = param_space)
+                     path_metrics = path_metrics)
     # metrics = task_evaluate_model(model, X_test, y_test)
     return clean_data_path
 
@@ -94,22 +117,7 @@ if __name__ == "__main__":
             'bucket': os.getenv('BUCKET'),
             'path_artifacts': os.getenv('PATH_ARTIFACTS'),
             'path_models': os.getenv('PATH_MODELS'),
-            'path_metrics': os.getenv('PATH_METRICS'),
-            'models': {
-                    "LogisticRegression": LogisticRegression(max_iter=1000, random_state=42),
-                    "RandomForest": RandomForestClassifier(random_state=42),
-                    "XGBoost": XGBClassifier(use_label_encoder=False, eval_metric='logloss', random_state=42),
-                    "LightGBM": LGBMClassifier(random_state=42)
-                },
-            'param_space' : {
-                    'max_depth': scope.int(hp.quniform('max_depth', 4, 15, 1)),
-                    'learning_rate': hp.loguniform('learning_rate', -3, 0),         # ~0.05–1.0
-                    'reg_alpha': hp.loguniform('reg_alpha', -5, -1),
-                    'reg_lambda': hp.loguniform('reg_lambda', -6, -1),
-                    'min_child_weight': hp.loguniform('min_child_weight', -1, 3),
-                    'subsample': hp.uniform('subsample', 0.6, 1.0),
-                    'colsample_bytree': hp.uniform('colsample_bytree', 0.6, 1.0)
-                }
+            'path_metrics': os.getenv('PATH_METRICS')
         } 
     )
 
